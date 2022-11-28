bb7593b1b6ad23e94daef80fc411fd0784bb45b0
Amirhosein-JVD <102900476+Amirhosein-JVD@users.noreply.github.com>
2022-11-29 02:05:13 +0330

    InfixToPostfix Algorithm

---
+++ src/PostfixToPrefix.py
@@ -0,0 +1,13 @@
+from Functions import *
+
+
+def PostfixToPrefix(exp):
+    operands = []
+    for i in exp:
+        if not isOperator(i):
+            operands.append(i)
+        else:
+            temp = ''
+            temp = i + operands.pop(-2) + operands.pop(-1)
+            operands.append(temp)
+    return operands.pop()
